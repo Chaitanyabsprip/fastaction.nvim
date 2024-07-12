78309d62b0c1c56d8169934454ea2a8f98d7ef1f
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-07-12 16:14:23 +0530

    feat: add type for configuration

--- lua/lsp-fastaction/types.lua
+++ lua/lsp-fastaction/types.lua
@@ -6,7 +6,7 @@
 ---@class ActionConfig
 ---@field pattern string
 ---@field key string
----@field order integer
+---@field order? integer
 
 ---@class WindowOpts
 ---@field title? string
@@ -26,3 +26,17 @@
 ---@field prompt? string
 ---@field format_item? fun(item: any): string
 ---@field kind? string
+
+---@class FastActionConfig
+---This configures options for the popup used by code action popup as well as
+---the select popup.
+---@field popup? PopupConfig
+---@field priority? table<string, ActionConfig[]>
+
+---@class PopupConfig
+---@field title? string
+---@field relative? string
+---@field border? string | string[]
+---@field hide_cursor? boolean
+---@field highlight? table<string, string>
+---@field dismiss_keys? string[]
