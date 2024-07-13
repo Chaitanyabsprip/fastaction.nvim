02a60e420f3ced9973743bd2201c62961dcc6820
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-07-13 15:02:26 +0530

    fix(lsp): resolve code actions when necessary

--- lua/fastaction/lsp.lua
+++ lua/fastaction/lsp.lua
@@ -1,9 +1,14 @@
 local M = {}
+local m = {}
+
+m.textDocument_codeAction = "textDocument/codeAction"
+m.codeAction_resolve = "codeAction/resolve"
 
 ---@return CodeAction[] | nil
 local request_code_action = function(params)
+	local buffer = vim.api.nvim_get_current_buf()
 	---@type table<integer, {result: lsp.CodeAction[], error: lsp.ResponseError? }>?, string?
-	local results_lsp, err = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 10000)
+	local results_lsp, err = vim.lsp.buf_request_sync(buffer, "textDocument/codeAction", params, 10000)
 	if err then
 		return vim.notify("ERROR: " .. err, vim.log.levels.ERROR)
 	end
@@ -18,9 +23,11 @@ local request_code_action = function(params)
 				---@class lsp.CodeAction
 				---@field client_id integer
 				---@field client_name string
+				---@field buffer integer
 				local res = result
 				res.client_id = client_id
 				res.client_name = client and client.name or ""
+				res.buffer = buffer
 				table.insert(commands, result)
 			end
 		end
@@ -46,17 +53,41 @@ function M.range_code_action()
 	return request_code_action(params)
 end
 
+---@param action lsp.Command|lsp.CodeAction
+---@param client vim.lsp.Client
+---@param ctx { bufnr: integer }
+function m.apply_action(action, client, ctx)
+	if action.edit then
+		vim.lsp.util.apply_workspace_edit(action.edit, client.offset_encoding)
+	end
+	local a_cmd = action.command
+	if a_cmd then
+		local command = type(a_cmd) == "table" and a_cmd or action --[[@as lsp.Command]]
+		client:_exec_cmd(command, ctx)
+	end
+end
+
+---@param action CodeAction
 function M.execute_command(action)
-	local offset_encoding = vim.lsp.get_client_by_id(action.client_id).offset_encoding
-	if action.edit or type(action.command) == "table" then
-		if action.edit then
-			vim.lsp.util.apply_workspace_edit(action.edit, offset_encoding)
-		end
-		if type(action.command) == "table" then
-			vim.lsp.buf.execute_command(action.command)
-		end
+	local client = assert(vim.lsp.get_client_by_id(action.client_id))
+	local ctx = { bufnr = action.buffer }
+	local reg = client.dynamic_capabilities:get(m.textDocument_codeAction, { bufnr = action.buffer })
+	local supports_resolve = vim.tbl_get(reg or {}, "registerOptions", "resolveProvider")
+		or client.supports_method(m.codeAction_resolve)
+	if not action.edit and client and supports_resolve then
+		client.request(m.codeAction_resolve, action, function(err, resolved_action)
+			if err then
+				if action.command then
+					m.apply_action(action, client, ctx)
+				else
+					vim.notify(err.code .. ": " .. err.message, vim.log.levels.ERROR)
+				end
+			else
+				m.apply_action(resolved_action, client, ctx)
+			end
+		end, action.buffer)
 	else
-		vim.lsp.buf.execute_command(action)
+		m.apply_action(action, client, ctx)
 	end
 end
 
