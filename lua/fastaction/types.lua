77ecb320980ce5fcb6337bbeb42714ecac2ebb2f
Michael Bøcker-Larsen <mbl@code.boutique>
2025-04-25 23:51:36 +0800

    fix: make brackets optional
    The newly added feature added a "brackets" key to the opts table,
    however not as optional. This commit makes it "brackets" optional.

--- lua/fastaction/types.lua
+++ lua/fastaction/types.lua
@@ -49,7 +49,7 @@
 ---`vim.ui.select`
 ---@field fallback_threshold? integer
 ---@field format_right_section? fun(item: LspCodeActionItem): string
----@field brackets table<string, string>
+---@field brackets? table<string, string>
 
 ---@class PopupConfig
 ---Title of the popup.
